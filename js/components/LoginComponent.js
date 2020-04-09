b0d833ffbfdc3cfa449bbcac37488543204555d1
Dana Marin <d_maringarcia@fanshaweonline.ca>
2020-04-09 16:21:13 -0400

    Adding new start point + admin functionality

--- js/components/LoginComponent.js
+++ js/components/LoginComponent.js
@@ -1,13 +1,13 @@
 export default {
     template: `
-        <section id="login-container">
+        <div id="container">
             <div id="help">
                 <a href="help.html">Help</a>
             </div>
             <div id="stacked-logo">
                 <img src="images/logo-stacked.svg" alt="Roku Flashback Logo">
             </div>
-            <form @submit.prevent="login">
+            <form>
                 <div class="login-form">
                     <div class="username-field">
                         <label for="inlineFormInputName"></label>
@@ -20,14 +20,14 @@ export default {
                     </div>
 
                     <div class="submit-button">
-                        <button type="submit">Sign In</button>
+                        <button v-on:click.prevent="login()" type="submit">Sign In</button>
                     </div>
                 </div>
                 <div id="new-to-roku">
                     <a href="sign-up.html">New to Roku? Sign up now.</a>
                 </div>
             </form>  
-        </section>
+        </div>
      `,
 
     data() {
@@ -42,39 +42,37 @@ export default {
 
     methods: {
         login() {
-            //console.log(this.$parent.mockAccount.username);
-            // debugger;
+
             if (this.input.username != "" && this.input.password != "") {
-                // use the FormData object to collect and send our params
+                // fetch the user from the DB
+                // generate the form data
                 let formData = new FormData();
 
                 formData.append("username", this.input.username);
                 formData.append("password", this.input.password);
 
-                let url = "./includes/index.php?user=true";
+                let url = `./admin/admin_login.php`;
 
                 fetch(url, {
-                    method: "POST",
+                    method: 'POST',
                     body: formData
                 })
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data);
-
-                    // tell the app that we have a successful login
-                    // and store the user object that we retrieved
-
-                    // true below means that authentication worked
-                    // data is the user we retrieved from the DB
-                    this.$emit("authenticated", true, data[0]);
-
-                    // push the user to the users page
-                    this.$router.replace({name: "users"});                    
-                })
-                .catch((err) => console.log(err));
-
+                    .then(res => res.json())
+                    .then(data => {
+                        if (typeof data != "object") { // means that we're not getting a user object back
+                            console.warn(data);
+                            // just for testing
+                            alert("authentication failed, please try again");
+                        } else {
+                            this.$emit("authenticated", true, data);
+                            this.$router.replace({ name: "users" });
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
             } else {
-                console.error("inputs can't be blank!");
+                console.log("A username and password must be present");
             }
         }
     }
