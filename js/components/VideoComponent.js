486021a7a63b023694138a03c7fbf118eaae3e67
Sujin Lee <sujinlee@Sujins-MacBook-Pro-2.local>
2020-04-11 21:56:25 -0400

    adding class to js

--- js/components/VideoComponent.js
+++ js/components/VideoComponent.js
@@ -16,20 +16,39 @@ export default {
                 </div>
             </div>
             
-
             <div class="col-12 order-1 order-md-2 col-md-9 media-container">
                 <video autoplay controls muted :src="'images/' + currentMediaDetails.movies_trailer" class="fs-video"></video>
             </div>
         </div>
-
+        <div class="col-12 col-sm-9 media-info">
+            <ul class="media-genres">
+                <li>
+                    <a href="horror" @click.prevent="filterMedia('horror')">Horror</a>
+                </li>
+                <li>
+                    <a href="comedy" @click.prevent="filterMedia('comedy')">Comedy</a>
+                </li>
+                <li>
+                    <a href="family" @click.prevent="filterMedia('family')">Family</a>
+                </li>
+                <li>
+                    <a href="horror" @click.prevent="filterMedia('fantasy')">Fantasy</a>
+                </li>
+                <li>
+                    <a href="horror" @click.prevent="retrieveVideoContent">All</a>
+                </li>
+            </ul>
+        </div>
         <div class="row">
-            <div class="col-12 col-sm-9">
-                <div class="thumb-wrapper clearfix">
-                    <img v-for="item in allRetrievedVideos" :src="'images/' + item.movies_cover" alt="media
-                    thumb" @click="loadNewMovie(item)" class="img-thumbnail rounded float-left
-                    media-thumb">
+            <div class="coverS">
+                <div class="col-12 col-sm-9 posterC">
+                    <div class="thumb-wrapper clearfix">
+                        <img v-for="item in allRetrievedVideos" :src="'images/' + item.movies_cover" alt="media
+                        thumb" @click="loadNewMovie(item)" class="img-thumbnail rounded float-left
+                        media-thumb">
+                    </div>
                 </div>
-            </div>
+            </div>    
         </div>
     </section>
     `,
@@ -46,11 +65,30 @@ export default {
     },
 
     methods: {
+        filterMedia(filter) {
+            // debugger;
+
+            let url = `./admin/index.php?media=movies&filter=${filter}`;
+
+            fetch(url)
+                .then(res => res.json())
+                .then(data => {
+                    this.allRetrievedVideos = data;
+                    this.currentMediaDetails = data[0];
+                })
+
+        },
+
         retrieveVideoContent() {
             // fetch all the video content here (don't care about filtering, genre etc at this point)
             // debugger;
 
             if (localStorage.getItem("cachedVideo")) {
+                this.allRetrievedVideos = JSON.parse(localStorage.getItem("cachedVideo"));
+
+                this.currentMediaDetails = this.allRetrievedVideos[0];
+
+            } else {
                 let url = `./admin/index.php?media=movies`;
 
                 fetch(url)
@@ -61,10 +99,6 @@ export default {
                         this.allRetrievedVideos = data;
                         this.currentMediaDetails = data[0];
                     })
-
-            } else {
-                this.allRetrievedVideos = JSON.parse(localStorage.getItem("cachedVideo"));
-                this.currentMediaDetails = this.allRetrievedVideos[0];
             }
 
         },
