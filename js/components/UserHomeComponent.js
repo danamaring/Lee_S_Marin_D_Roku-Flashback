89c0648939aafbc89a14758c533fc74225828103
Sujin Lee <sujinlee@Sujins-MacBook-Pro-2.local>
2020-04-11 21:55:58 -0400

    adding class to js

--- js/components/UserHomeComponent.js
+++ js/components/UserHomeComponent.js
@@ -15,17 +15,19 @@ export default {
 
         <!-- show media icons here -->
         <div class="row"> <!-- 2-up for nav and media info -->
-            <nav class="col-12 col-sm-3 side-nav">
-                <ul class="media-type">
-                    <li v-for="media in mediaTypes" :data-type="media.description" @click="switchMedia(media.component)">
-                        <span>
-                            <i v-bind:class="[media.iconClass]"></i>
-                        </span>
-                        
-                        <span class="d-none d-md-block">{{ media.description }}</span>
-                    </li>
-                </ul>
-            </nav>
+            <div class="side-left">
+                <nav class="col-12 col-sm-3 side-nav">
+                    <ul class="media-type">
+                        <li v-for="media in mediaTypes" :data-type="media.description" @click="switchMedia(media.component)">
+                            <span>
+                                <i v-bind:class="[media.iconClass]"></i>
+                            </span>
+                            
+                            <span class="d-none d-md-block">{{ media.description }}</span>
+                        </li>
+                    </ul>
+                </nav>
+            </div>    
         </div>
     </div>
     `,
